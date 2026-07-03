acd9d84defbee63a4b055475392929e8e61a78f0
studioandriolo-oss <studioandriolo@gmail.com>
2026-07-03 23:27:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ with col_input:
         "A - Solo lievi imprecisioni (Tolleranze < 2-5%)",
         "B - Spostamento o creazione di stanze normali",
         "C - Modifica o creazione di Bagni/Cucine",
-        "D - Demolizione muri spessi o portanti (Open Space)"
+        "D - Demolizione muri spessi o portanti o modifiche a parti strutturali"
     ])
     
     esterna = st.selectbox("2. SITUAZIONE ESTERNA / FACCIATE (Scegli la più impattante):", [
